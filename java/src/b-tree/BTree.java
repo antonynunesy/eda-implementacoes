eb47c98071696cb35eda177d734a0f253443369a
victorrafaelufcg <victor.rafael.soares.ferreira@ccc.ufcg.edu.br>
2026-07-21 18:49:35 -0300

    Separação comentada e BNode implementado

--- java/src/b-tree/BTree.java
+++ java/src/b-tree/BTree.java
@@ -1,44 +1,81 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class BTree{
 
+    private BNode root;
+
+    public BTree(){
+        this.root = null;
+    }
+
+    //VICTOR
     public void insert(int value){
     }
 
+    //VICTOR
     public boolean isEmpty(){
+        return root == null;
     }
 
+    //ANTONY
     public int height(){
+        return 0;
     }
 
+    //ANTONY
     public BNode search(int value){
+        return null;
     }
 
+    //VICTOR
     public BNode max(){
+        return null;
     }
 
+    //VICTOR
     public BNode min(){
+        return null;
     }
 
+    //ANTONY
     public void remove(int value){
     }
 
+    //VICTOR
     public ArrayList<BNode> depthFS(){
+        return null;
     }
 
+    //ANTONY
     public ArrayList<BNode> breadthFS(){
+        return null;
     }
 
+    //ANTONY
     public int size(){
+        return 0;
+    }
+    
+    //VICTOR
+    private void split(BNode node){
     }
 
-    private void split(Bnode node){
-    } 
-
+    //ANTONY
     private BNode getRoot(){
+        return root;
     }
 
 }
-public class BNode{
+
+//VICTOR
+class BNode{
     
+    private BNode parent;
+    private ArrayList<Integer> keys;
+    private ArrayList<BNode> children;
+    
+    public BNode(BNode parent){
+        this.keys = new ArrayList<>();
+        this.children = new ArrayList<>();
+    }
 }
