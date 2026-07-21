d062756125931e3284ced2faf3261cd1d6ccc87c
antonynunesy <antonygabrielnunes383@gmail.com>
2026-07-21 10:49:17 -0300

    to-do Methods

---
+++ java/src/b-tree/BTree.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class BTree{
+
+    public void insert(int value){
+    }
+
+    public boolean isEmpty(){
+    }
+
+    public int height(){
+    }
+
+    public BNode search(int value){
+    }
+
+    public BNode max(){
+    }
+
+    public BNode min(){
+    }
+
+    public void remove(int value){
+    }
+
+    public ArrayList<BNode> depthFS(){
+    }
+
+    public ArrayList<BNode> breadthFS(){
+    }
+
+    public int size(){
+    }
+
+    private void split(Bnode node){
+    } 
+
+    private BNode getRoot(){
+    }
+
+}
+public class BNode{
+    
+}
